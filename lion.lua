22802c7269c6f08cc09cf755e4258871267f2aba
Akiteru <akiteru@live.com>
2025-05-10 11:52:04 -0230

    Add script

---
+++ lion.lua
@@ -0,0 +1,257 @@
+-- Written by Akiteru with some code taken from Pasky13 and FractalFusion --
+
+-- Small dirty edit by SBDWolf to make it work with the bsnesv115+ core (set memory domain to WRAM)
+
+local function isGenesis()
+	return gameinfo.getromhash() == "E37BF6813648EC5E8BDBD2F7070010F8";
+end
+
+-- Util functions --
+local function read_u8(addr)
+	return memory.read_u8(addr);
+end
+local function read_u16(addr)
+	return memory.read_u16_le(addr);
+end
+local function isInactiveGeyser(addr)
+	return read_u16(addr + 0x30) == 0xDC17;
+end
+local function isActiveGeyser(addr)
+	return read_u16(addr + 0x30) == 0xD8CC;
+end
+if (isGenesis()) then
+	read_u16 = function(addr)
+		return memory.read_u16_be(addr);
+	end
+	isInactiveGeyser = function(addr)
+		return read_u16(addr + 0x32) == 0x35F0;
+	end
+	isActiveGeyser = function(addr)
+		return read_u16(addr + 0x32) == 0x3270;
+	end
+end
+-- Util functions end--
+
+-- Constants --
+local LINKED_START = 0x76;
+local LINKED_END = 0x78;
+local LINKED_FREE = 0x7A;
+local SIMBA_X = 0xB21B;
+local SIMBA_Y = 0xB21D;
+local RNG_A = 0x1E00;
+local RNG_B = 0x1E01;
+local RNG_C = 0x1E02;
+local RNG_ROLL = 0xC0CBBA;
+if (isGenesis()) then
+	-- Set correct addresses for Genesis
+	LINKED_START = 0xC350;
+	LINKED_END = 0xC352;
+	LINKED_FREE = 0xC34E;
+	SIMBA_X = 0x93D6;
+	SIMBA_Y = 0x93D8;
+	RNG_A = 0x81AF;
+	RNG_B = 0x81AE;
+	RNG_C = 0x81AD;
+	RNG_ROLL = 0x000B28;
+end
+-- Constants end --
+
+-- Set memory domain --
+if (isGenesis()) then
+	memory.usememorydomain("68K RAM");
+else
+	memory.usememorydomain("WRAM");
+end
+-- Set memory domain end --
+
+-- Some helper function stuff --
+local hexDigit = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+local xm;
+local ym;
+local xmOff;
+local ymOff;
+-- Some helper function stuff end --
+
+-- (Partially) implement some 65C816 instructions for the RNG simulator --
+local function lsr(val, mask)
+    mask = mask or 0xFF;
+    val = val & mask;
+    local next = val >> 1;
+    local carry = val & 1;
+    return {next, carry};
+end
+
+local function rol(val, carry, mask)
+    mask = mask or 0xFF;
+    val = val & mask;
+	local nextCarry = (val & 0x80) >> 7;
+	local next = ((val << 1) & mask) | carry;
+    return {next, nextCarry};
+end
+
+local function ror(val, carry, mask)
+    mask = mask or 0xFF;
+    val = val & mask;
+    local nextCarry = val & 1;
+	local next = (val >> 1) | (carry << 7);
+    return {next, nextCarry};
+end
+
+local function sbc(acc, val, carry, mask)
+    mask = mask or 0xFF;
+	acc = acc & mask;
+	val = val & mask;
+    local nextCarry = 0;
+    if acc >= val then
+        nextCarry = 1;
+    end
+	local next = (acc - val - 1 + carry) & mask;
+    return {next, nextCarry};
+end
+-- (Partially) implement some 65C816 instructions for the RNG simulator end --
+
+local function scaler()
+	xmOff = client.borderwidth();
+	ymOff = client.borderheight();
+	xm = (client.screenwidth() - 2 * xmOff) / 256;
+	ym = (client.screenheight() - 2 * ymOff) / 224;
+end
+
+local function hexStr(val, length)
+    length = length or 4;
+    local str = "";
+    for i = 1,length do
+        str = hexDigit[val%16+1] .. str;
+        val = math.floor(val / 16);
+    end
+    return str;
+end
+
+local function text(xPos, yPos, str)
+    gui.text(xPos * xm + xmOff, yPos * ym + ymOff, str);
+end
+
+-- Simulates a roll of the RNG and returns the new seed, as well as the value that would be written to $a68e at the end of $c0cbba --
+local function rngSim(a, b, c)
+    a = a or read_u8(RNG_A);
+    b = b or read_u8(RNG_B);
+    c = c or read_u8(RNG_C);
+    local acc = a;
+    local temp = nil;
+    local carry = nil;
+    temp = lsr(acc); acc = temp[1]; carry = temp[2];
+    temp = lsr(acc); acc = temp[1]; carry = temp[2];
+    temp = rol(c, carry); c = temp[1]; carry = temp[2];
+    temp = rol(b, carry); b = temp[1]; carry = temp[2];
+    temp = sbc(acc, a, carry); acc = temp[1]; carry = temp[2];
+    temp = lsr(acc); acc = temp[1]; carry = temp[2];
+    temp = ror(a, carry); a = temp[1]; carry = temp[2];
+    acc = a;
+    acc = acc ~ b;
+    local result = acc & 0xFF;
+    return {a, b, c, result};
+end
+
+-- Simulate a calculation of a geyser countdown initialization value based on the given RNG value (found at $c2dc6b or so) --
+local function geyserSim(val)
+    return (val & 0xF) + 0x32;
+end
+
+local function printLinked()
+    local x = 10;
+    local y = 25;
+    text(x, y, "Next free: " .. hexStr(read_u16(LINKED_FREE)));
+    y = y + 10;
+    local addr = read_u16(LINKED_START);
+    local i = 1;
+    while addr ~= 0 do
+        local extra = "";
+        -- These cases are for geysers
+        if isInactiveGeyser(addr) then
+            extra = "(" .. read_u16(addr + 0x4C) .. " R, " .. hexStr(read_u16(addr + 0x46), 2) .. " CD)";
+        elseif isActiveGeyser(addr) then
+            extra = "(" .. read_u16(addr + 0x4C) .. " R, " .. hexStr(read_u16(addr + 0x46), 2) .. " CD: OK)";
+        end
+        text(x, y, i .. ": " .. hexStr(addr) .. " " .. extra);
+        y = y + 10;
+        addr = read_u16(addr + 2);
+        i = i + 1;
+    end
+end
+
+local prevA = nil;
+local prevB = nil;
+local prevC = nil;
+local rngCount = 0;
+local rngCountStr = "";
+local function printData()
+    local x = 100;
+    local y = 25;
+    local a = read_u8(RNG_A);
+    local b = read_u8(RNG_B);
+    local c = read_u8(RNG_C);
+    if prevA ~= nil and (a ~= prevA or b ~= prevB or c ~= prevC) then
+        rngCountStr = "Num. rolls: " .. rngCount;
+    end
+    text(x, y, "Curr. seed: " .. hexStr(a, 2) .. " " .. hexStr(b, 2) .. " " .. hexStr(c, 2));
+    y = y + 10;
+    text(x, y, "Nexts: ");
+    x = x + 30;
+    prevA = a;
+    prevB = b;
+    prevC = c;
+    local geyserStr = "Geyser CDs: ";
+    for i = 1,10 do
+        local sim = rngSim(a, b, c);
+        text(x, y, hexStr(sim[4], 2) .. " (Seed: " .. hexStr(sim[1], 2) .. " " .. hexStr(sim[2], 2) .. " " .. hexStr(sim[3], 2) .. ")");
+        y = y + 10;
+        a = sim[1];
+        b = sim[2];
+        c = sim[3];
+        if i <= 4 then
+            geyserStr = geyserStr .. hexStr(geyserSim(sim[4]), 2);
+            if i <= 3 then
+                geyserStr = geyserStr .. ", ";
+            end
+        end
+    end
+    x = x - 20;
+    if rngCountStr ~= "" then
+        text(x, y, rngCountStr);
+        y = y + 10;
+    end
+    text(x, y, geyserStr);
+	y = y + 10;
+    local simbaX = read_u16(SIMBA_X);
+    local pixel = "o o o o";
+    if (simbaX % 4 == 0) then
+        pixel = "x o o o";
+    end
+    if (simbaX % 4 == 1) then
+        pixel = "o x o o";
+    end
+    if (simbaX % 4 == 2) then
+        pixel = "o o x o";
+    end
+    if (simbaX % 4 == 3) then
+        pixel = "o o o x";
+    end
+    text(x, y, "Pixel: " .. pixel);
+    y = y + 10;
+end
+
+local function resetCount()
+    rngCount = 0;
+end
+local function incCount()
+    rngCount = rngCount + 1;
+end
+event.onframestart(resetCount);
+event.onmemoryexecute(incCount, RNG_ROLL);
+
+while true do
+    scaler();
+    printLinked();
+    printData();
+    emu.frameadvance();
+end
